1e7e6f97122dfebccd6a1ffb95c0520a11209ed5
Omede Success <successomede06@gmail.com>
2026-01-15 02:19:06 +0100

    Real Estate Landing Page With React

---
+++ myApp/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Landing } from './component/Landing'
+
+function App() {
+ 
+
+  return (
+    <>
+      <Landing/>
+    </>
+  )
+}
+
+export default App
